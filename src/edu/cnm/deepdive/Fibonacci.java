d06e11ca3ac453352dbdda98c17d76ae8c1f7587
Michael Lacey <laceymwes@gmail.com>
2018-05-30 05:05:48 -0600

    Widen Fibonacci iterative return type to BigInteger.

--- src/edu/cnm/deepdive/Fibonacci.java
+++ src/edu/cnm/deepdive/Fibonacci.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive;
 
+import java.math.BigInteger;
+
 public class Fibonacci {
 
   public static long recursive(int generation){
@@ -9,20 +11,17 @@ public class Fibonacci {
     return recursive(generation - 1) + recursive(generation - 2);
   }
 
-  public static long iterative(int generation){
+  public static BigInteger iterative(int generation){
     long prev2 = 1;
     long prev1 = 1;
-    if(generation == 0) {
-      return prev2;
-    }
-    if(generation == 1){
-      return prev1;
+    if(generation <= 1){
+      return BigInteger.ONE;
     }
     for(int i = 2; i <= generation; i++){
       long current = prev1 + prev2;
       prev2 = prev1;
       prev1 = current;
     }
-    return prev1;
+    return BigInteger.valueOf(prev1);
   }
 }
